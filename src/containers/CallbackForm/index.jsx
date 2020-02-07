c3f82260555bafca3e8b351894c4d29eae25ba9d
Fxva Skin <fxva@outlook.com>
2020-02-07 10:07:35 +0600

    phone is required | callback function wip

--- src/containers/CallbackForm/index.jsx
+++ src/containers/CallbackForm/index.jsx
@@ -8,17 +8,21 @@ import { Input } from 'components/Input'
 import { Button } from 'components/Button'
 
 const CallbackForm = ({ className, ...props }) => {
-  const handleSubmit = event => {
+  const handleSubmit = async event => {
     event.preventDefault()
-    console.log('call back me please!11')
+
+    const response = await fetch('http://localhost:34567/send')
+    const data = await response.json()
+
+    console.log(data)
   }
   return (
     <Card className={cx(className, styles.card)} {...props}>
       <Header as="h3">Форма обратной связи</Header>
       <p>Оставьте свой номер телефона и мы вам перезвоним</p>
       <form onSubmit={handleSubmit} className={cx(styles.form)}>
-        <Input name="kek" label="Ваше имя" />
-        <Input name="shrek" label="Номер телефона" />
+        <Input name="name" label="Ваше имя" />
+        <Input name="phone" label="Номер телефона" required />
         <Button type="submit" className={styles.button}>
           Позвоните мне
         </Button>
